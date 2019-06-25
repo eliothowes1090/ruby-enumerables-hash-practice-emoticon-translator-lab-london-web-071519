a0be6b80b497b7999ca820ee6a6fde54bfce4aea
Eliot Howes <ejhowes@gmail.com>
2019-06-25 13:52:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(yaml, emoticon)
   ordered_library.each do |dict, emoticons|
     if dict == "get_emoticon"
       emoticons.each do |english, japanese|
-        if emoticon == English
+        if emoticon == english
           japanese
         else
           "Sorry, that emoticon was not found"
