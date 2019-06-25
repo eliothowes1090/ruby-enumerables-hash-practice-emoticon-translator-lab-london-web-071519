962c265274fd88c127e641d377c30e8473d34a1f
Eliot Howes <ejhowes@gmail.com>
2019-06-25 13:42:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,33 @@
 # require modules here
 
-def load_library
+require 'yaml'
+require 'pry'
+
+def load_library(yaml)
   # code goes here
+  library = YAML.load_file(yaml)
+  ordered_library = {"get_meaning" => {}, "get_emoticon" => {}}
+
+  library.each do |name, emoticon|
+    ordered_library["get_meaning"][emoticon[1]] = name
+    ordered_library["get_emoticon"][emoticon[0]] = emoticon[1]
+  end
+
+ordered_library
+
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(yaml, emoticon)
   # code goes here
+  ordered_library = load_library(yaml)
+  ordered_library.each_key do |dict|
+    if dict == "get_emoticon"
+      dict.each do |japanese, english|
+binding.pry
+      end
+    end
+  end
+
 end
 
 def get_english_meaning
